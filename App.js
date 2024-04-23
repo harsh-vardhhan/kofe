4a27a461b873914091bcbc18c8f14b97896b56e8
Harsh Vardhan <harsh.vardhan.611@gmail.com>
2024-04-23 23:14:01 +0530

    quiz card

--- App.js
+++ App.js
@@ -1,21 +1,56 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
-import { Button } from "react-native-paper";
+import { StyleSheet, View, Image } from "react-native";
+import { Card, Text } from "react-native-paper";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Button
-        icon="camera"
-        mode="contained"
-        onPress={() => console.log("Pressed")}
+      <View
+        style={{
+          flexDirection: "row",
+          flexWrap: "wrap",
+          flex: 1,
+          alignItems: "flex-start",
+          marginTop: 50,
+        }}
       >
-        Press me
-      </Button>
+        <QuizCard />
+        <QuizCard />
+        <QuizCard />
+        <QuizCard />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
 }
+
+const QuizCard = () => {
+  return (
+    <Card
+      style={{
+        width: "42%",
+        marginLeft: 20,
+        marginTop: 20,
+        height: 200,
+        backgroundColor: "white",
+      }}
+    >
+      <View>
+        <Image
+          style={{
+            height: 100,
+            width: 90,
+            margin: 10,
+            marginLeft: 40,
+            marginTop: 40,
+          }}
+          source={require("./assets/images/apple.png")}
+        />
+        <Text style={{ marginLeft: 60 }}>apple</Text>
+      </View>
+    </Card>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
